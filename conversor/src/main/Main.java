7c0ba9a6aadddf7d8e68822cd068ecaedea93370
Gerardo Leivas <gdleivas@gmail.com>
2023-09-02 18:45:23 -0300

    commit 1

---
+++ conversor/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import services.Menu;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.Mostar();
+    }
+}
